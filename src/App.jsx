fd0201e9884d5234e5e9e02c1aec2d1a51bfd5d8
Dikhtiar Tania <you@example.com>
2024-05-20 15:50:58 +0300

    new file

--- src/App.jsx
+++ src/App.jsx
@@ -5,26 +5,40 @@
 // import PageCounter from "./components/CounterComponent/Counter/PageCounter";
 
 import { useEffect, useState } from "react";
+// import Timer from "./components/Timer/Timer";
 
 function App() {
- const [counter, setCounter] = useState(0);
- const [counter2, setCounter2] = useState(0);
- // console.log('clg')
-  // console.log('without useEf counter:', counter)
+  // const [isShowTimer, setIsShowTimer] = useState(false)
+  const [counter, setCounter] = useState(0);
+  const [counter2, setCounter2] = useState(()=>{
+    const localCounter = localStorage.getItem('counter2');
+    return localCounter ? JSON.parse(localCounter): 0
+  });
+  //  // console.log('clg')
+  //   // console.log('without useEf counter:', counter)
+  
+  // const total = counter2 + 100;
 
-  useEffect(()=>{
-    console.log('current counter:', counter)
-  },[counter]);
+  // useEffect(()=>{
+  //   console.log('current total:', total);
+  //   console.log('current counter:', counter)
+  // },[counter, total]);
 
-  useEffect(()=>{
-    console.log('current counter2:', counter2)
-  },[counter2]);
+  useEffect(() => {
+    localStorage.setItem("counter2:", counter2);
+    console.log('effect')
+  }, [counter2]);
 
-  return (<div>
-    <button onClick={()=>setCounter(counter+1)}>{counter}</button>
-    <button onClick={()=>setCounter2(counter2+1)}>{counter2}</button>
-  </div>
-)}
+  return (
+    <div>
+      {console.log('render')}
+      <button onClick={() => setCounter(counter + 1)}>{counter}</button>
+      <button onClick={() => setCounter2(counter2 + 1)}>{counter2}</button>
+      {/* <button onClick={()=> setIsShowTimer(!isShowTimer)}>{isShowTimer? 'Hide':'Show'}</button>
+    {isShowTimer && <Timer/>} */}
+    </div>
+  );
+}
 export default App;
 ///////////////////////////////////////////////////////////////////////////////
 // function App() {
@@ -33,7 +47,7 @@ export default App;
 //   const isLastPage = page === post.length - 1;//або через if
 //   const isFirstPage = page === 0;
 
-//   const handleChangePage = (params) => { 
+//   const handleChangePage = (params) => {
 //     if(params === 'prev'){
 //       // if(page === 0) return
 //       setPage ( page - 1);
@@ -54,16 +68,15 @@ export default App;
 // }
 // export default App;
 
-
-  // const handleClick = (id) => {
-  //   console.log("id:", id);
-  // };
-  // return (
-  //   <div>
-  //     <button onClick={handleClick}>qwerty</button>
-  //     <button onClick={() => {handleClick(10)}}>qwerty</button>
-  //   </div>
-  // );
+// const handleClick = (id) => {
+//   console.log("id:", id);
+// };
+// return (
+//   <div>
+//     <button onClick={handleClick}>qwerty</button>
+//     <button onClick={() => {handleClick(10)}}>qwerty</button>
+//   </div>
+// );
 
 // const CounterComponentWithState = () => {
 //   const [counter, setCounter] = useState(0);
@@ -78,10 +91,4 @@ export default App;
 //   </>
 // }
 
-
-
 /////////////////////////////////////////////////////
-
-
-
-
